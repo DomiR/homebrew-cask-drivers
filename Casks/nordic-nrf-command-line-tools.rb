87c88fe5bfa2e61221e437fa3414d44fd385ade6
Dominique Rau <domi.github@gmail.com>
2022-03-17 22:33:16 +0100

    Update nordic-nrf-command-line-tools.rb

--- Casks/nordic-nrf-command-line-tools.rb
+++ Casks/nordic-nrf-command-line-tools.rb
@@ -1,6 +1,6 @@
 cask "nordic-nrf-command-line-tools" do
-  version "10.13.0"
-  sha256 "29b8824be08acbd7215bcb5de77f156660a35c982e3e2a669e1abf3924d4d158"
+  version "10.15.4"
+  sha256 "819BCB09B6F0AFAF83755CF564B682FB425096FC6A363B7F7705EDCE450EE2A4"
 
   url "https://www.nordicsemi.com/-/media/Software-and-other-downloads/Desktop-software/nRF-command-line-tools/sw/Versions-#{version.major}-x-x/#{version.dots_to_hyphens}/nRF-Command-Line-Tools_#{version.dots_to_underscores}_OSX.zip"
   name "nRF Command Line Tools"
